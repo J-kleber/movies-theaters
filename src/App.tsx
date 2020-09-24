9d40c8d0c04c77ffa8847e7ce8700abc289729f5
unknown <john@emporioht.com.br>
2020-09-24 16:05:45 -0300

    Rotas: criando configuração da página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { View } from 'react-native';
+import Routes from './routes';
 
 const App: React.FC = () => {
-  return <View></View>;
+  return (
+    <NavigationContainer>
+      <Routes />
+    </NavigationContainer>
+  );
 };
 
 export default App;
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,22 @@
+import React, { ReactElement } from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Dashboard from '../pages/Dashboard';
+
+const Stack = createStackNavigator();
+
+const AppRoutes: React.FC = () => (
+  <Stack.Navigator
+    screenOptions={{
+      headerShown: false,
+      cardStyle: {
+        backgroundColor: '#ffffff',
+      },
+    }}
+    initialRouteName="Dashboard"
+  >
+    <Stack.Screen name="Dashboard" component={Dashboard} />
+  </Stack.Navigator>
+);
+
+export default AppRoutes;
